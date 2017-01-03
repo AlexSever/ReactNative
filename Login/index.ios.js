d909c48c3bab219d8d5ec069deb736a1896fa073
AlexSever <alexei.miasin@gmail.com>
2017-01-03 13:41:24 -0500

    Learning REDUX in TechStack Project

--- Login/index.ios.js
+++ Login/index.ios.js
@@ -1,3 +1,4 @@
+// [MyProject]/node_modules/react-native/React/Base/RCTPlatform.m:26:10:
 // Commented "case UIUserInterfaceIdiomCarPlay"
 
 import { AppRegistry } from 'react-native';
---
+++ TechStack/src/components/common/Button.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+      <TouchableOpacity onPress={onPress} style={buttonStyle}>
+          <Text style={textStyle}>
+              {children}
+          </Text>
+      </TouchableOpacity>
+  );
+};
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonStyle: {
+        height: 40,
+        width: 100,
+        // flex: 1,
+        // flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        //alignSelf: 'stretch',
+        alignSelf: 'center',
+        borderRadius: 5,
+        borderWidth: 1,
+        backgroundColor: '#007aff',
+        borderColor: '#007aff',
+        marginTop: 20,
+        marginBottom: 10
+    }
+};
+
+export { Button };
